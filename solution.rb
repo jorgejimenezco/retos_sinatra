b831b0910b1a7d6b96090e1e5e31edfeb0586a85
Jorge Jimenez <stellajimenez@Apple-Mac.local>
2017-02-23 20:12:58 -0500

    sinatra  2

--- solution.rb
+++ solution.rb
@@ -1,9 +1,16 @@
 require 'sinatra'
 
-get '/' do
-  if params[:nombre] && params[:nombre] != ""
-    "<h1>Hola #{params[:nombre]}!<h1>"
+# get '/' do
+get '/makers/:nombre' do
+
+    if params[:nombre] && params[:nombre] != ""
+    "<h1>Hola #{params[:nombre].capitalize}!</h1>"
   else
     "<h1>Hola desconocido!</h1>"
   end
 end
+
+#     "<h1>Hola #{params[:nombre]}!<h1>"
+
+#   end
+# end
